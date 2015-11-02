9a4f17eb4348b2163783ed2183db41c357b234d6
PMLaw <miruappy@gmail.com>
2015-11-02 22:52:30 +0800

    changes to box.cpp

--- src/SceneObjects/Box.cpp
+++ src/SceneObjects/Box.cpp
@@ -8,6 +8,7 @@ bool Box::intersectLocal( const ray& r, isect& i ) const {
 	double size = 0.5;
 	vec3f Xo = r.getPosition(), Xd = r.getDirection();
 
+	// check if the light ray pass through the bounding box
 	for (int j = 0; j < 3; j++) {
 		if (Xd[j] == 0) {
 			if (Xo[j] < -size || Xo[j] > size)
@@ -22,17 +23,13 @@ bool Box::intersectLocal( const ray& r, isect& i ) const {
 				Tnear = T1;
 			if (T2 < Tfar) /* want smallest Tfar */
 				Tfar = T2;
+			if (Tnear > Tfar || Tfar < RAY_EPSILON)
+				return false;
 		}
 	}
 
-	if (Tnear > Tfar)
-		return false;
-
-	if (Tfar < RAY_EPSILON)
-		return false;
-
 	i.obj = this;
-	if (Tnear < RAY_EPSILON)
+	if (Tnear < RAY_EPSILON) // only need to check if less than 0 as ray always starts from (0, 0, 0)
 		i.t = Tfar;
 	else
 		i.t = Tnear;
